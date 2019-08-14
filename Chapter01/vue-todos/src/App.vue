b4dcfe1b7f608b02d410e1cb2c6cf3a599e85221
CoderDream <coderdream@gmail.com>
2019-08-14 11:08:44 +0800

    1.2　数据绑定 20190814

--- Chapter01/vue-todos/src/App.vue
+++ Chapter01/vue-todos/src/App.vue
@@ -1,6 +1,25 @@
 <template>
   <div id="app">
     <h1>{{ title }}</h1>
+    <ul>
+      <li v-for="todo in todos">
+        <label>{{ todo.value }}</label>
+      </li>
+    </ul>
+    <hr>
+    <ul>
+      <li v-for="(todo,index) in todos"
+          :id="index">
+        <label>{{ index + 1 }}.{{ todo.value }}</label>
+      </li>
+    </ul>
+    <hr>
+    <!-- v-for不单单可以循环渲染数组，还可以渲染对象属性，例如：-->
+    <ul>
+      <li v-for="value in object">
+        {{ value }}
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -11,7 +30,16 @@
     name: 'app',
     data () {
       return {
-        title: "vue-todos"
+        title: "vue-todos",
+        todos: [
+          { value: "阅读一本关于前端开发的书", done: false },
+          { value: "补充范例代码", done: true },
+          { value: "写心得", done: false }
+        ],
+        object: {
+          first_name : "Ray",
+          last_name : "Liang"
+        }
       }
     }
   }
